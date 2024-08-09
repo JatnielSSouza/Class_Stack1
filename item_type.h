d58d1b7aac8593ccf3928c784e791cd20ebf28e2
JatnielSSouza <jstz1120@gmail.com>
2024-08-09 06:09:14 -0300

    Correção do comentário no arquivo intem_type.h

--- item_type.h
+++ item_type.h
@@ -1,3 +1,3 @@
 // Declaração:
-// Tipo abstrato de dados:
-typedef char ItemType;
+// Definição de um novo nome para um determinado tipo:
+typedef char ItemType; // Sintaxe: typedef antigo_nome novo_nome;
